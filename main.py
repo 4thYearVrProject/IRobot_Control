f577727510f69ff4aa2d6448e7099b6a3696e848
liustone99 <79604926+liustone99@users.noreply.github.com>
2022-03-04 14:50:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,26 @@ def main():
     robot = Irobot()
     robot.start()
     
+    while robot.init == True
+        # Movement controls
+        direction = input("Please enter direction (options include 'forward', 'backward', 'left', right', 'left180' and 'right180')")
+
+        if direction == "forward":
+            robot.Drive_forward(1)
+        elif direction == "backward":
+            robot.Drive_backward(1)
+        elif direction == "left":
+            robot.Turn_left()
+        elif direction == "right":
+            robot.Turn_right()
+        elif direction == "left180":
+            robot.turn_backwards_CW()
+        elif direction == "right180":
+            robot.turn_backwards_CCW()
+        else:
+            print("Invalid input, try again")
+    
+    
 
 if __name__ == "__main__":
     main()
