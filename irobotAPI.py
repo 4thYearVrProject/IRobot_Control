fc5d68b0b6e8972d09d6a803edb45c72b9b92a59
liustone99 <stoneliu@cmail.carleton.ca>
2022-03-09 19:38:26 -0500

    Updated with fixes

--- irobotAPI.py
+++ irobotAPI.py
@@ -1,14 +1,14 @@
 from  pycreate2 import Create2
 import time
 
-class Irobot();
-    def __init__(self,portNumber);
+class Irobot():
+    def __init__(self,portNumber):
         self.port = 0
         self.bot = 0
         self.init = False
         self.portNumber = portNumber
 
-    def Start(self,portNumber):
+    def start(self):
         self.port = "/dev/" + self.portNumber  # where is your serial port?
         self.bot = Create2(self.port)
         # Start the Create 2
@@ -16,7 +16,7 @@ class Irobot();
         self.init = True
         print("Robot started")
 
-    def Stop(self):
+    def stop(self):
         if(self.init):
             self.bot.drive_stop()
 
@@ -35,8 +35,8 @@ class Irobot();
     def Drive_forward(self, distance):
         if(self.init):
             self.bot.drive_direct(100,100)
-             #if 1s = 0.5 m, then t = 2*d
-             time.sleep(distance*2) # modify 2 later
+            #if 1s = 0.5 m, then t = 2*d
+            time.sleep(distance*2) # modify 2 later
 
     #Drive backward by a specified distance
     def Drive_backward(self, distance):
@@ -46,13 +46,13 @@ class Irobot();
             time.sleep(distance*2)
 
     #Turn left 90 degrees (CW)   
-    def Turn_left(self, distance):
+    def Turn_left(self):
         if(self.init):
             self.bot.drive_direct(180,-180)
             time.sleep(1)
 
     #Turn right 90 degrees (CCW)    
-    def Turn_right(self, distance):
+    def Turn_right(self):
         if(self.init):
             self.bot.drive_direct(-180,180)
             time.sleep(1)
